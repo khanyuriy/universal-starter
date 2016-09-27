9112c4f7e1688d7dfc48cbc63f6f673b524a4ea4
Yuriy Khan <khan.yuriy@gmail.com>
2016-09-27 22:14:53 +0300

    code to reproduce the bug

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app',
   template: `
   <p>Hello Angular Universal App</p>
+  <p>{{text}}</p>
   <router-outlet></router-outlet>
   `
 })
 export class App {
+  text: string;
 
+  constructor(private http: Http) {
+    http.get('/assets/data.txt').subscribe(response => {
+      this.text = response.text();
+    })
+  }
 }
 
 @Component({
